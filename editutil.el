23b07455986e2f81606fbf8e1605e5efb1b74ee1
Syohei YOSHIDA <syohex@gmail.com>
2014-03-06 00:34:45 +0900

    Fix binding for moving by char

--- editutil.el
+++ editutil.el
@@ -467,8 +467,8 @@
   (global-set-key [(control shift up)] 'editutil-move-line-up)
   (global-set-key [(control shift down)] 'editutil-move-line-down)
 
-  (global-set-key (kbd "M-e") 'editutil-forward-char)
-  (global-set-key (kbd "M-a") 'editutil-backward-char)
+  (global-set-key (kbd "M-q") 'editutil-forward-char)
+  (global-set-key (kbd "M-Q") 'editutil-backward-char)
   (global-set-key (kbd "C-M-u") 'editutil-backward-up)
   (global-set-key (kbd "C-M-r") 'editutil-escape-parentheses)
   (global-set-key (kbd "C-M-n") 'editutil-forward-list)
